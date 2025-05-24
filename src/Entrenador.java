a96409a86f9650d4211d51ee3d31dbbf51f0ff4f
Xumi22 <davidgallego20@gmail.com>
2025-05-24 16:14:23 +0200

    Entrenador creado

---
+++ src/Entrenador.java
@@ -0,0 +1,5 @@
+public class Entrenador {
+    public static void main(String[] args) {
+        System.out.println("Corree");
+    }
+}
--- src/Jugador.java
+++ src/Jugador.java
@@ -1,4 +1,4 @@
-public class jugador {
+public class Jugador {
     public static void main(String[] args) {
         System.out.println( "suuuu");
     }
