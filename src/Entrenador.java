08808542a0b3c7e16a88fac5b390741b23efbd00
Xumi22 <davidgallego20@gmail.com>
2025-05-24 16:34:21 +0200

    añadir muchachos Entrenador

--- src/Entrenador.java
+++ src/Entrenador.java
@@ -1,6 +1,6 @@
 public class Entrenador {
     public static void main(String[] args) {
         System.out.println("Correeeee");
-        System.out.println("vamos chicos");
+        System.out.println("vamos chicos correr muchachos");
     }
 }
